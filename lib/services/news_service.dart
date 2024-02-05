7f8a77f69b4c3294444e5bc0fceffb3701b470ad
chamaxx9zp <chamakapiyumal@gmail.com>
2024-02-05 12:44:34 +0530

    fix API

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -8,7 +8,7 @@ class NewsService {
   final String apiKey = '4e78306cc76e4cdc813358fc7a613c2b'; // Replace with your News API key
 
   Future<List<NewsArticle>> getNews() async {
-    final response = await http.get(Uri.parse('GET GET https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'));
+    final response = await http.get(Uri.parse('https://newsapi.org/v2/top-headlines/sources?apiKey=API_KEY'));
 
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body)['articles'];
